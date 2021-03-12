4781ddf6ccf3c81ef68151e17edb4b2b044a5342
Hazel Bounpraseuth <78515992+bounpraseuthh9003@users.noreply.github.com>
2021-03-12 13:02:06 +1300

    RPS game component

---
+++ 01_RPS_Base_Component.py
@@ -0,0 +1,20 @@
+# import random
+
+# Functions go here
+
+
+# Main routine goes here
+
+# Lists of valid responses
+yes_no_list = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+# Ask users if they have played before
+# if 'yes', show instructions
+
+# Ask users for # of rounds then loop...
+
+# Ask user if they want to see their game history
+# if 'yes' show game history
+
+# Show game statistics
